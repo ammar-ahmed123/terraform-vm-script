70a7a1a91789d8f9dc6bc1d9c8353c5eea73911b
Ammar Ahmed <ammarejaz1874@gmail.com>
2021-06-25 00:40:56 +0500

    Update main.tf

--- task1/main.tf
+++ task1/main.tf
@@ -12,3 +12,13 @@ resource "azurerm_resource_group" "rg" {
   }
 }
 
+#create a terraform tf file
+terraform {
+  backend "azurerm" {
+    resource_group_name   = "kopicloud-tstate-rg"
+    storage_account_name  = "kopicloudtfstate8132"
+    container_name        = "tfstate1"
+    key                   = "tfstatefiles"
+  }
+}
+#access_key: gkMFKVItRS4wpaYtiwA8XZ2wiq/kmsb5fX/01QXDb/qu+2Ppg50QM3PJ/KLDfey5UIb79nOs09XBvNavm70gbA==
