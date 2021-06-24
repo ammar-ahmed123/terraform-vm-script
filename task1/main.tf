3367e528c497b08096ebd6f1908615a4213c0fd0
Ammar Ahmed <ammarejaz1874@gmail.com>
2021-06-24 20:13:16 +0500

    Update main.tf

--- task1/main.tf
+++ task1/main.tf
@@ -12,19 +12,3 @@ resource "azurerm_resource_group" "rg" {
   }
 }
 
-data "azurerm_key_vault_secret" "example" {
-  name         = "virtualmachinename"
-  key_vault_id = "d508624f-a0b7-4fd3-9511-05b18ca02784"
-}
-
-output "secret_value" {
-  value = data.azurerm_key_vault_secret.example.value
-}
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.resource_group_location
-
-  tags = {
-    environment = output.secret_value.value
-  }
-}
